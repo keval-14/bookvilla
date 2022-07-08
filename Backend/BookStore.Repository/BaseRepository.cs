ecf1d7ea84f6dbe1703eb0f815c7e00784604aa0
Keval Mangukiya <work.kevalpatel@gmail.com>
2022-07-08 11:57:37 +0530

    code upload

---
+++ Backend/BookStore.Repository/BaseRepository.cs
@@ -0,0 +1,9 @@
+﻿using BookStore.Models.DataModels;
+
+namespace BookStore.Repository
+{
+    public class BaseRepository
+    {
+        protected readonly tatvadbContext _context = new tatvadbContext();
+    }
+}
